d91cd5a79e31f95e6237c81ccef556f1e223b7e6
orionhead <63302426+orionhead@users.noreply.github.com>
2020-06-17 08:11:26 -0500

    Object oriented program 1st batch

---
+++ main_file.py
@@ -0,0 +1,7 @@
+from heroes import SuperHeroes
+
+hero_one = SuperHeroes("iron man")
+print(hero_one.convert_superhero_name())
+
+hero_object2 = SuperHeroes("ant man")
+print(hero_object2.convert_superhero_name())
